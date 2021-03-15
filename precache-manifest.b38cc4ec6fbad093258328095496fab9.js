eecf782583feb46272c875e0ca07efa7a6cafdca
Dima3939 <wanteddima@gmail.com>
2021-03-15 16:06:54 +0200

    init

---
+++ precache-manifest.b38cc4ec6fbad093258328095496fab9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9523bce2d15952c3a12",
+    "url": "vue_giphy_api/css/app.15a3cba3.css"
+  },
+  {
+    "revision": "f3b2580355d42519ddd6",
+    "url": "vue_giphy_api/css/chunk-vendors.a2520d52.css"
+  },
+  {
+    "revision": "33712184d33d70a4dd75ae6fc7bcb6f3",
+    "url": "vue_giphy_api/index.html"
+  },
+  {
+    "revision": "a9523bce2d15952c3a12",
+    "url": "vue_giphy_api/js/app.0ad1d36e.js"
+  },
+  {
+    "revision": "f3b2580355d42519ddd6",
+    "url": "vue_giphy_api/js/chunk-vendors.287c187c.js"
+  },
+  {
+    "revision": "e901dcbf4d4d4b2cb72494041b81ff54",
+    "url": "vue_giphy_api/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "vue_giphy_api/robots.txt"
+  }
+]);
